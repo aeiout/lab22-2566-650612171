3f9454c1495a67256cb6fbfc48bbea421703e030
Your Name <ueaarthon13117@gmail.com>
2023-09-30 13:32:18 +0700

    finished

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650612171"
+                fullName="Ueaarthorn Uawongtrakul"
                 year="2023"
               />
             </Container>
